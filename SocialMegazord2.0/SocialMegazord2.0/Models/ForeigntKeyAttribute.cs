b228b1c17da57589c7dccab91b17cbc9d4aecdce
TerranMarine <terranmarinesc@abv.bg>
2016-12-17 21:05:35 +0200

    Added Post db table

--- SocialMegazord2.0/SocialMegazord2.0/Models/ForeigntKeyAttribute.cs
+++ SocialMegazord2.0/SocialMegazord2.0/Models/ForeigntKeyAttribute.cs
@@ -4,5 +4,11 @@ namespace SocialMegazord2._0.Models
 {
     internal class ForeigntKeyAttribute : Attribute
     {
+        private string v;
+
+        public ForeigntKeyAttribute(string v)
+        {
+            this.v = v;
+        }
     }
 }
---
+++ SocialMegazord2.0/SocialMegazord2.0/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialMegazord2._0.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+
+        public string Content { get; set; }
+
+        [ForeigntKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
